5558c9f7b2caf993fe99502487656b97a6e69f22
Rajesh <13061978+code-with-rj1399@users.noreply.github.com>
2026-08-22 18:59:02 +0530

    Tools information in response

--- src/main/java/com/codewithrj/agentic_ai/tools/QAAgent.java
+++ src/main/java/com/codewithrj/agentic_ai/tools/QAAgent.java
@@ -3,6 +3,7 @@ package com.codewithrj.agentic_ai.tools;
 
 import com.codewithrj.agentic_ai.tools.models.WikipediaTool;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.model.ChatResponse;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -27,6 +28,48 @@ public class QAAgent {
                 .tools(budgetSearchTool, mathematicalTool, wikipediaTool)
                 .call();
 
-        return callResponseSpec.content() +"\n\n\n" + callResponseSpec.chatResponse();
+        return callResponseSpec.content() +"\n\n\n" + formatChatResponse(callResponseSpec.chatResponse());
+    }
+
+    private String formatChatResponse(ChatResponse chatResponse) {
+
+        if(chatResponse == null)
+                return "";
+
+        StringBuilder result = new StringBuilder();
+
+        result.append("─── Agent Details ───\n\n");
+
+        result.append("Tool Calls:\n");
+
+        chatResponse.getResult()
+                .getOutput()
+                .getToolCalls()
+                .forEach(toolCall -> {
+
+                    result.append("🔧 ")
+                            .append(toolCall.name())
+                            .append("\n");
+
+                    result.append("   Arguments: ")
+                            .append(toolCall.arguments())
+                            .append("\n\n");
+                });
+
+        result.append("Metadata:\n");
+
+        result.append("   Prompt Tokens: ")
+                .append(chatResponse.getMetadata().getUsage().getPromptTokens())
+                .append("\n");
+
+        result.append("   Completion Tokens: ")
+                .append(chatResponse.getMetadata().getUsage().getCompletionTokens())
+                .append("\n");
+
+        result.append("   Total Tokens: ")
+                .append(chatResponse.getMetadata().getUsage().getTotalTokens())
+                .append("\n");
+
+        return result.toString();
     }
 }
